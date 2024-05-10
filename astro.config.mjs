b042c0361dac1c26c1479411b58f6ca865d69b73
blackcat <blackcat_19216811@163.com>
2024-05-10 20:30:13 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,8 +30,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://blackcat-love.github.io",
+  base: "/blackcatLove",
   trailingSlash: "always",
   integrations: [
     tailwind(),
